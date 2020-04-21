e9a959d721c104ddd1b70d955fdd037d0eb1ab2e
Fauzan Fathurrahman <fauzan08fauzan@gmail.com>
2020-04-21 16:15:21 +0700

    Add Mongo

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
+const mongoUri = "mongodb+srv://cnq:53u2wBMu9iKBxDu4@cluster0-7b4g1.mongodb.net/test?retryWrites=true&w=majority"
+
 app.get('/',(req,res)=>{
     res.send("Welcome to node js")
 })
